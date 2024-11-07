e238d5b2805c31d4f92ce51af2b471fb443b25d7
Bryan Knox <bryan@knoxbits.com>
2024-11-07 07:07:30 -0800

    Add OnConfigurationReloaded

--- WpfBlazor/App.xaml.cs
+++ WpfBlazor/App.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
-using System.IO;
+using Microsoft.Extensions.Primitives;
 using System.Windows;
 
 namespace WpfBlazor;
@@ -9,14 +9,29 @@ namespace WpfBlazor;
 /// </summary>
 public partial class App : Application
 {
-        public IConfiguration Configuration { get; }
+    public IConfiguration Configuration { get; }
 
-        public App()
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+    public App()
+    {
+        // Load configuration from appsettings.json in the directory
+        // where the application is running.
+        var builder = new ConfigurationBuilder()
+                        .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
 
-            Configuration = builder.Build();
-        }
+        Configuration = builder.Build();
+
+        // Register a callback to be called after a change in the the configuration
+        // (appsettings.json) has been detected and the configuration has been reloaded.
+        ChangeToken.OnChange(() => Configuration.GetReloadToken(), OnConfigurationReloaded);
+    }
+
+    /// <summary>
+    /// Event handler called after a change in the the configuration
+    /// (appsettings.json) has been detected and the configuration has been reloaded.
+    /// </summary>
+    private void OnConfigurationReloaded()
+    {
+        MessageBox.Show("Configuration (appsettings.json) has been reloaded.");
+    }
 }
