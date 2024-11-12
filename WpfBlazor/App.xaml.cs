6652c9223d8ea56a00820b9c27fa928c9016946e
Bryan Knox <bryan@knoxbits.com>
2024-11-12 08:48:36 -0800

    Dependency Injection setup out of App

--- WpfBlazor/App.xaml.cs
+++ WpfBlazor/App.xaml.cs
@@ -11,61 +11,10 @@ namespace WpfBlazor;
 /// </summary>
 public partial class App : Application
 {
-    private IConfiguration Configuration { get; }
-
     public IServiceProvider ServiceProvider { get; }
 
-    public App()
+    public App(IServiceProvider serviceProvider)
     {
-        try
-        {
-            var host = CreateHostBuilder().Build();
-            ServiceProvider = host.Services;
-            Configuration = ServiceProvider.GetRequiredService<IConfiguration>();
-            RegisterConfigurationChangeReloadHandler();
-        }
-        catch (Exception ex)
-        {
-            MessageBox.Show(
-                $"An error occurred:\n\n{ex.GetType()}\n\n{ex.Message}\n\nThe application cannot be started.",
-                "Error Configuring the App",
-                MessageBoxButton.OK,
-                MessageBoxImage.Error);
-
-            // Rethrow the exception to terminate the application.
-            throw;
-        }
-    }
-
-    private IHostBuilder CreateHostBuilder()
-    {
-        IHostBuilder hostBuilder = Host.CreateDefaultBuilder();
-
-        hostBuilder.ConfigureAppConfiguration((context, config) =>
-        {
-            config.SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
-                  .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-        });
-
-        hostBuilder.ConfigureServices((context, services) =>
-        {
-            // NOTE: IConfiguration is registered above by CreateDefaultBuilder().
-
-            services.AddWpfBlazorWebView();
-        });
-
-        return hostBuilder;
-    }
-
-    private void RegisterConfigurationChangeReloadHandler()
-    {
-        // Register a callback to be called after a change in the configuration
-        // (appsettings.json) has been detected and the configuration has been reloaded.
-        ChangeToken.OnChange(() => Configuration.GetReloadToken(), OnConfigurationReloaded);
-    }
-
-    private void OnConfigurationReloaded()
-    {
-        MessageBox.Show("Configuration (appsettings.json) has been reloaded.");
+        ServiceProvider = serviceProvider;
     }
 }
--- WpfBlazor/Program.cs
+++ WpfBlazor/Program.cs
@@ -1,11 +1,39 @@
-﻿namespace WpfBlazor;
+﻿using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace WpfBlazor;
 
 public class Program
 {
     [STAThread]
     static void Main()
     {
-        var app = new App();
+        IServiceProvider serviceProvider;
+        try
+        {
+            serviceProvider = DependencyInjection.Configure();
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(
+                $"An error occurred:\n\n{ex.GetType()}\n\n{ex.Message}\n\nThe application cannot be started.",
+                "Error Configuring the App",
+                MessageBoxButton.OK,
+                MessageBoxImage.Error);
+
+            // Rethrow the exception to terminate the application.
+            throw;
+        }
+
+        RunWpfApp(serviceProvider);
+    }
+
+    // Ensure this method is not inlined, so that no WPF assemblies are loaded
+    // before this method is called from the Main method.
+    [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+    static void RunWpfApp(IServiceProvider serviceProvider)
+    {
+        var app = new App(serviceProvider);
         app.InitializeComponent();
         app.Run();
     }
