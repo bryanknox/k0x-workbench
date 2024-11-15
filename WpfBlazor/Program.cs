50e1ed2f2003633c9e7cd67dcbc54b8fb7030c48
Bryan Knox <bryan@knoxbits.com>
2024-11-15 07:52:33 -0800

    Ran Code Cleanup

--- WpfBlazor/Program.cs
+++ WpfBlazor/Program.cs
@@ -1,7 +1,7 @@
-﻿using System.Runtime.CompilerServices;
-using System.Windows;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System.Runtime.CompilerServices;
+using System.Windows;
 
 namespace WpfBlazor;
 
