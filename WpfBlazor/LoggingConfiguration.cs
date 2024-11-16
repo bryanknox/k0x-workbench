b26eb099f379bbc04947997589516367cf0dc7d9
Bryan Knox <bryan@knoxbits.com>
2024-11-16 06:56:32 -0800

    Set Serilog MinimumLevel from appsettings.json

--- WpfBlazor/LoggingConfiguration.cs
+++ WpfBlazor/LoggingConfiguration.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Serilog;
+using Serilog.Events;
 using System.IO;
 
 namespace WpfBlazor;
@@ -23,8 +24,22 @@ internal static class LoggingConfiguration
             logDirectoryPath,
             $"log_{DateTime.Now:yyyyMMdd_HHmmss}.log");
 
+        // Get the Logging.LogLevel.Default value from the configuration
+        var logLevel = configuration.GetValue<string>("Logging:LogLevel:Default");
+        var serilogLogLevel = logLevel switch
+        {
+            "Trace" => LogEventLevel.Verbose,
+            "Debug" => LogEventLevel.Debug,
+            "Information" => LogEventLevel.Information,
+            "Warning" => LogEventLevel.Warning,
+            "Error" => LogEventLevel.Error,
+            "Critical" => LogEventLevel.Fatal,
+            _ => LogEventLevel.Information
+        };
+
         Log.Logger = new LoggerConfiguration()
             .ReadFrom.Configuration(configuration)
+            .MinimumLevel.Is(serilogLogLevel)
             .WriteTo.Console()
             .WriteTo.File(
                 logFilePath,
