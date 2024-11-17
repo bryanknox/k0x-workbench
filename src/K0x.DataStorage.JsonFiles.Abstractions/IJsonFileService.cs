154e10052aa7adb67b3296b8b7d41ea69a7b4364
Bryan Knox <bryan@knoxbits.com>
2024-11-17 06:57:46 -0800

    Add K0x.DataStorage.JsonFiles modified from SinceWhen

---
+++ src/K0x.DataStorage.JsonFiles.Abstractions/IJsonFileService.cs
@@ -0,0 +1,7 @@
+﻿namespace K0x.DataStorage.JsonFiles.Abstractions;
+
+public interface IJsonFileService<T>
+{
+    Task<T> LoadAsync(string filePath);
+    Task SaveAsync(T data, string filePath);
+}
---
+++ src/K0x.DataStorage.JsonFiles/JsonFileService.cs
@@ -0,0 +1,40 @@
+﻿using K0x.DataStorage.JsonFiles.Abstractions;
+using System.Text.Json;
+
+namespace K0x.DataStorage.JsonFiles;
+
+public class JsonFileService<T> : IJsonFileService<T>
+{
+    [System.Diagnostics.CodeAnalysis.SuppressMessage(
+        "Performance",
+        "CA1822:Mark members as static",
+        Justification = "Instances of this class are used in dependency injection.")]
+    public async Task<T> LoadAsync(string filePath)
+    {
+        using var sr = File.OpenText(filePath);
+
+        string json = await sr.ReadToEndAsync();
+
+        T? data = JsonSerializer.Deserialize<T>(json);
+
+        if (data is null)
+        {
+            throw new InvalidDataException($"File contains no data. FilePath: {filePath}");
+        }
+
+        return data;
+    }
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage(
+        "Performance",
+        "CA1822:Mark members as static",
+        Justification = "Instances of this class are used in dependency injection.")]
+    public async Task SaveAsync(T data, string filePath)
+    {
+        string json = JsonSerializer.Serialize(data);
+
+        using var sw = new StreamWriter(filePath);
+
+        await sw.WriteAsync(json);
+    }
+}
