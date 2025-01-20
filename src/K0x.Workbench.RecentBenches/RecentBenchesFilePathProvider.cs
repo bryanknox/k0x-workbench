cf8fdac9d56db919b64187e05c22fac5f1a8e61d
Bryan Knox <bryan@knoxbits.com>
2025-01-20 07:55:47 -0800

    IRecentBenchesFilePathProvider WIP

---
+++ src/K0x.Workbench.RecentBenches/RecentBenchesFilePathProvider.cs
@@ -0,0 +1,13 @@
+﻿using K0x.Workbench.RecentBenches.Abstractions;
+
+namespace K0x.Workbench.RecentBenches;
+
+public class RecentBenchesFilePathProvider : IRecentBenchesFilePathProvider
+{
+    public Task<string> GetFilePathAsync()
+    {
+        // TODO: Load the file path from the configuration
+
+        throw new NotImplementedException();
+    }
+}
