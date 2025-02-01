d483ab7f008fa7453e2854021b2b754cc4421899
Bryan Knox <bryan@knoxbits.com>
2025-02-01 15:13:38 -0800

    Add RecentPage stub

---
+++ src/WpfBlazor/Pages/RecentPage.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
+
+namespace WpfBlazor.Pages;
+
+public partial class RecentPage : ComponentBase
+{
+    [Inject]
+    private ILogger<RecentPage> Logger { get; set; } = default!;
+
+    protected override async Task OnInitializedAsync()
+    {
+        Logger.LogTrace("OnInitializedAsync START.");
+
+        await base.OnInitializedAsync();
+
+        Logger.LogTrace("OnInitializedAsync END.");
+    }
+
+}
