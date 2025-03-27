5cbe5772278d9ec760045552fd86aad5e866092a
Bryan Knox <bryan@knoxbits.com>
2025-03-27 15:32:47 -0700

    Add RecentPage sytle and file edit

--- src/WpfBlazor/Pages/RecentPage.razor.cs
+++ src/WpfBlazor/Pages/RecentPage.razor.cs
@@ -2,20 +2,24 @@ using K0x.Workbench.DataStorage.Abstractions;
 using K0x.Workbench.RecentBenches.Abstractions;
 using K0x.Workbench.RecentBenches.Abstractions.Models;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.Extensions.Logging;
+using System.Windows;
 
 namespace WpfBlazor.Pages;
 
 public partial class RecentPage : ComponentBase
 {
     [Inject]
+    IBenchFilePathProvider BenchFilePathProvider { get; set; } = default!;
+    [Inject]
     private ILogger<RecentPage> Logger { get; set; } = default!;
     [Inject]
-    private IRecentBenchesLoader RecentBenchesLoader { get; set; } = default!;
-    [Inject]
-    private IBenchFilePathProvider BenchFilePathProvider { get; set; } = default!;
-    [Inject]
     private NavigationManager NavigationManager { get; set; } = default!;
+    [Inject]
+    private IRecentBenchesFilePathProvider RecentBenchesFilePathProvider { get; set; } = default!;
+    [Inject]
+    private IRecentBenchesLoader RecentBenchesLoader { get; set; } = default!;
 
     private List<RecentBench> RecentBenches { get; set; } = new();
     private bool IsLoading { get; set; }
@@ -56,7 +60,7 @@ public partial class RecentPage : ComponentBase
         }
     }
 
-    private async Task RefreshRecentBenches()
+    private async Task ReloadRecentBenches()
     {
         await LoadRecentBenchesAsync();
     }
@@ -65,5 +69,35 @@ public partial class RecentPage : ComponentBase
     {
         BenchFilePathProvider.FilePath = bench.FilePath;
         NavigationManager.NavigateTo("/bench");
+    }
+        
+   private void EditRecentBenchesFile(MouseEventArgs e)
+    {
+        string? recentBenchesFilePath = RecentBenchesFilePathProvider.GetFilePath();
+        if (!string.IsNullOrEmpty(recentBenchesFilePath))
+        {
+            var psi = new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = recentBenchesFilePath,
+                UseShellExecute = true
+            };
+            try
+            {
+                System.Diagnostics.Process.Start(psi);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex, $"Error opening file for editing: {recentBenchesFilePath}");
+
+                MessageBox.Show(
+                    $"Error opening file for editing: {recentBenchesFilePath}\n"
+                    + $"{ex.GetType()}\n"
+                    + "\n"
+                    + $"{ex.Message}\n",
+                    caption: "Error opening file for editing",
+                    button: MessageBoxButton.OK,
+                    icon: MessageBoxImage.Error);
+            }
+        }
     }
 }
