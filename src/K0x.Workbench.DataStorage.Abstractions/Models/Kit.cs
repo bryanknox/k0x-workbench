2239dd9e6455516a0de032ed90ce4a79f133a605
Bryan Knox <bryanknoxbits@outlook.com>
2025-12-18 19:42:24 -0800

    Add nested kits

--- src/K0x.Workbench.DataStorage.Abstractions/Models/Kit.cs
+++ src/K0x.Workbench.DataStorage.Abstractions/Models/Kit.cs
@@ -4,5 +4,5 @@ public record Kit
 {
     public required string Label { get; init; }
     public IList<Tool> Tools { get; init; } = new List<Tool>();
-
+    public IList<Kit> Kits { get; init; } = new List<Kit>();
 }
